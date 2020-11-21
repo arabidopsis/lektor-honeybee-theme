9769bca1c309ef51cb7bc3a7038cea75ba5e00d3
Ian Castleden <ian.castleden@gmail.com>
2020-11-21 13:20:14 +0800

    added openlayer

---
+++ templates/shortcodes/omap.js
@@ -0,0 +1,26 @@
+/*{# https://openlayersbook.github.io/ch02-key-concepts-in-openlayers/example-02.html  #}*/
+function omap_layout(data) {
+    let {lon, lat, z, mapid} = data;
+    lat = +lat
+    lon = +lon
+    z = +z
+    const map = new ol.Map({
+        target: 'map-' + mapid,
+        layers: [
+            new ol.layer.Tile({
+            source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([lon,lat]),
+            zoom: z
+        })
+    });
+
+    const popup = new ol.Overlay({
+        element: document.getElementById('overlay-'+mapid),
+        positioning: 'bottom-center'
+    });
+    popup.setPosition(ol.proj.fromLonLat([lon,lat]));
+    map.addOverlay(popup);
+}
